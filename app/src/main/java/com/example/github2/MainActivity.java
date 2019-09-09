b1df59564bac023ad9982333b725c26643b3ffd2
Nafisa42 <n.tabassum290@gmail.com>
2019-09-09 12:46:03 +0600

    commit by a

---
+++ app/src/main/java/com/example/github2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.github2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//this is a commit by a
